aae64d715f663f55bee3d04cf4eee4a7cf1308b7
RhuanArquiles <rhuanarquiles07@gmail.com>
2023-11-07 21:29:07 -0300

    Criando topbar

---
+++ sport-recife/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { TopbarComponent } from './topbar/topbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidenavComponent,
+    TopbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
