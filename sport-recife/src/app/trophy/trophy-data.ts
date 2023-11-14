c2c8709341a4fa3013a517b348a6b289d42f385a
RhuanArquiles <rhuanarquiles07@gmail.com>
2023-11-14 00:19:10 -0300

    Adicionando outros títulos e um background

--- sport-recife/src/app/trophy/trophy-data.ts
+++ sport-recife/src/app/trophy/trophy-data.ts
@@ -8,12 +8,48 @@ export const trophyData = [
 
     },
 
-    // {
+    {
 
-    //     nomeTitulo: 'Copa do Brasil 2008',
-    //     divisao: 'Nacional',
-    //     imagem: '2008',
-    //     descricao:'Em 11 de junho de 2008, o Sport sagrava-se campeão da Copa do Brasil. A Ilha do Retiro ficou conhecida como “La Bombonilha” pela pressão que a torcida fez sobre os adversários em todo o torneio. Na finalíssima, Sport x Corinthians. A primeira partida foi em São Paulo, e o alvinegro venceu por 3×1. No entanto, o gol de Enílton, no finalzinho do segundo tempo, fez Carlinhos Bala profetizar: “Ele fez o gol do título!”. A previsão de Bala estava corretíssima. Com um gol que ele mesmo marcou e outro de Luciano Henrique, o Sport venceu o Corinthians por 2×0 no duelo da volta e conquistou o segundo troféu mais importante do País. Comandado por Nelsinho Baptista, o time do Sport na decisão foi escalado com: Magrão; Diogo, Durval, Igor e Dutra; Daniel Paulista, Sandro Goiano, Kássio e Luciano Henrique; Leandro Machado e Carlinhos Bala.'
+        nomeTitulo: 'Copa do Brasil 2008',
+        divisao: 'Nacional',
+        imagem: '2008',
+        descricao:'Em 11 de junho de 2008, o Sport sagrava-se campeão da Copa do Brasil. A Ilha do Retiro ficou conhecida como “La Bombonilha” pela pressão que a torcida fez sobre os adversários em todo o torneio. Na finalíssima, Sport x Corinthians. A primeira partida foi em São Paulo, e o alvinegro venceu por 3×1. No entanto, o gol de Enílton, no finalzinho do segundo tempo, fez Carlinhos Bala profetizar: “Ele fez o gol do título!”. A previsão de Bala estava corretíssima. Com um gol que ele mesmo marcou e outro de Luciano Henrique, o Sport venceu o Corinthians por 2×0 no duelo da volta e conquistou o segundo troféu mais importante do País. Comandado por Nelsinho Baptista, o time do Sport na decisão foi escalado com: Magrão; Diogo, Durval, Igor e Dutra; Daniel Paulista, Sandro Goiano, Kássio e Luciano Henrique; Leandro Machado e Carlinhos Bala.'
 
-    // }
+    },
+    
+    {
+
+        nomeTitulo: 'Brasileiro de 1990',
+        divisao: 'Série B',
+        imagem: '1990',
+        descricao:'Rebaixado em 1989, o Sport voltou à elite do futebol brasileiro já no ano seguinte, sendo campeão da Série B de 1990. Mais um troféu para a galeria do Rubro-negro Pernambucano, que vinha de um vice-campeonato da Copa do Brasil. Nas semifinais, o time leonino passou pelo Guarani. Na decisão da Segundona, contra o Atlético-PR, 1×1 em Curitiba e 0x0 na Ilha do Retiro. Na grande final, o Leão da Praça da Bandeira, treinado por Brida, foi escalado com: Paulo Victor; Lopes, Aílton, Márcio Alcântara e Glauco; Agnaldo, Alencar e Marcus Vinícius; Mirandinha, Luís Carlos e Neco.'
+
+    },
+
+    {
+
+        nomeTitulo: 'Copa do Nordeste (1994, 2000 e 2014)',
+        divisao: 'Regional',
+        imagem: 'tricamp',
+        descricao:'No torneio regional mais competitivo do País, o Sport tem seu lugar de destaque. Três vezes campeão da Copa do Nordeste, o Leão da Praça da Bandeira alcançou o primeiro título em 15 de dezembro de 1994, vencendo o CRB na decisão por pênaltis, em Alagoas. Seis anos depois, o Rubro-negro Pernambucano conquistava o bicampeonato. Foi Sangaletti quem marcou, de cabeça, o gol que garantiu o empate por 2×2 contra o Vitória, na Ilha do Retiro. Na ocasião, os leoninos tinham a melhor campanha da competição e jogavam pela igualdade no placar. Em 2014, o terceiro título. Após vencer o Ceará por 2×0 na partida de ida da final, o time rubro-negro empatou por 1×1 no jogo da volta, no Castelão. Com gol de Neto Baiano, de pênalti, o Sport garantiu o tricampeonato da Copa do Nordeste.'
+
+    },
+
+    {
+
+        nomeTitulo: 'Torneio Norte e Nordeste (1968)',
+        divisao: 'Regional',
+        imagem: 'torneionortenordeste1968',
+        descricao:'Reunindo equipes das regiões Norte e Nordeste do Brasil, esta competição foi realizada durante três anos (1968, 1969 e 1970), com o Sport sendo campeão na primeira edição. Com a finalíssima sendo disputada em 1969, o Rubro-negro Pernambucano encarou o Remo. No jogo de ida, no dia 23 de fevereiro, em Belém-PA, o Leão venceu por 3×1. No segundo duelo, em 2 de março, na Ilha do Retiro, mais uma vitória rubro-negra, desta vez por 2×1. No placar agregado, 5×2 para os leoninos, que garantiram a taça do interregional. O torneio foi organizado pela Confederação Brasileira de Desportos (CBD), com o intuito de movimentar os clubes naquela época.',
+
+    },
+
+    {
+
+        nomeTitulo: 'Campeonato Pernambucano (43 vezes campeão)',
+        divisao: 'Estadual',
+        imagem: 'pernambucano2010',
+        descricao: 'Hegemonia em Pernambuco. O Sport já levantou a taça do Estadual 43 vezes, estando bem à frente dos principais rivais. O Leão possui dois pentacampeonatos (1996-2000 e 2006-2010). De maneira invicta, o time rubro-negro alcançou o título por quatro vezes (1917, 1941, 1998 e 2009). O destaque vai para final de 98, contra o Porto, em 7 de junho, quando a Ilha do Retiro teve o seu maior público já registrado – contando apenas com torcedores rubro-negros. Na ocasião, 56.875 pessoas viram o atacante Irani marcar os dois gols da vitória por 2×0 contra o time caruaruense, garantindo, na época, o 31º título do Estadual. O Sport foi Campeão Pernambucano em 1916, 1917, 1920, 1923, 1924, 1925, 1928, 1938, 1941, 1942, 1943, 1948, 1949, 1953, 1955, 1956, 1958, 1961, 1962, 1975, 1977, 1980, 1981, 1982, 1988, 1991, 1992, 1994, 1996, 1997, 1998, 1999, 2000, 2003, 2006, 2007, 2008, 2009, 2010, 2014, 2017, 2019 e 2023',
+
+    }
 ]
