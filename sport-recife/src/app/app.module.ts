f8e914b7eab33d5789e41241e8c0f76b0edad29b
RhuanArquiles <rhuanarquiles07@gmail.com>
2023-11-13 22:31:00 -0300

    Adicionando botões na top-bar, importanto bibliotecas e adicionando accordion

--- sport-recife/src/app/app.module.ts
+++ sport-recife/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatExpansionModule} from '@angular/material/expansion';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +11,9 @@ import { TrophyComponent } from './trophy/trophy.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { HomeComponent } from './home/home.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 @NgModule({
@@ -27,7 +30,8 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatIconModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatButtonModule
 
 
   ],
